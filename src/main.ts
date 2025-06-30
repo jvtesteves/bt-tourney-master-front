f24093c56d3a2bf262b74f7581dd40002bcc9f39
RodrigoMaia03 <rodrigonaia2003@gmail.com>
2025-06-30 16:26:59 -0300

    Script src/main.ts atualizado

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,24 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import { Amplify } from 'aws-amplify'
 import App from './App.vue'
-import router from './router' // Importe o roteador
+import router from './router'
+
+// 1. Configuração da Amplify com a estrutura correta para a v6+
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      // ID do User Pool
+      userPoolId: 'sa-east-1_z5OvqHadk',
+
+      // ID do Cliente de Aplicação
+      userPoolClientId: '5qbfq0jtnbc0t0h04e0ou7nqs4',
+    }
+  },
+});
 
 const app = createApp(App)
-
-app.use(router) // Use o roteador na aplicação
+app.use(createPinia())
+app.use(router)
 app.mount('#app')
